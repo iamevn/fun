5fce093edac5bf2276238f73eec5aaba57251268
iamevn <evan@thinkpad>
2014-03-05 19:52:58 -0800

    asd

---
+++ euler/euler_17.rkt
@@ -0,0 +1,57 @@
+(define names (list "" "thousand" "million" "billion" "trillion" "quadrillion" "quintillion" "sextillion" "septillion" "octillion" "nonillion" "decillion" "undecillion" "duodecillion" "tredecillion" "quattuordecillion" "qinquadecillion" "sedecillion" "septendecillion" "octodecillion" "novendecillion" "vigntillion" "unvigintillion" "duovigintillion" "tresvigintillion" "quattuorvigintillion" "quinquavigintillion" "sesvigintillion" "octovigintillion" "novemvigintillion" "trigintillion" "untrigintillion" "duotrigintillion"))
+(define <20 (list "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve" "thirteen" "fourteen" "fifteen" "sixteen" "seventeen" "eighteen" "nineteen"))
+(define tens>20 (list "twenty" "thirty" "fourty" "fifty" "sixty" "seventy" "eighty" "nintey"))
+
+(define (to-word<20 n)
+  (let loop ((n n) (ls <20))
+    (if (= n 1) (car ls)
+      (loop (- n 1) (cdr ls)))))
+
+(define (to-word-tens n)
+  (let loop ((n n) (ls tens>20))
+    (if (= n 2) (car ls)
+      (loop (- n 1) (cdr ls)))))
+
+(define (name<1000 n) 
+  (let loop ((n n) (ret '()))
+    (cond ((= n 0) '(zero))
+          ((> (quotient n 100) 0)
+           (cons (to-word<20 (quotient n 100))
+                 (cons "hundred" 
+                       (cond ((= (remainder n 100) 0) 
+                              '())
+                             ((< (remainder n 100) 20)
+                              (cons "and" (cons (to-word<20 (remainder n 100)) ret)))
+                             (else
+                               (cons "and" (cons (to-word-tens (quotient (remainder n 100) 10)) (if (= (remainder n 10) 0) ret
+                                                                                                  (cons (to-word<20 (remainder n 10)) ret)))))))))
+          ((< (remainder n 100) 20)
+           (cons (to-word<20 (remainder n 100)) ret))
+          (else
+            (cons (to-word-tens (quotient (remainder n 100) 10)) (if (= (remainder n 10) 0) ret
+                                                                   (cons (to-word<20 (remainder n 10)) ret)))))))
+
+(define (number-name n)
+  (if (= n 0) '(zero)
+    (let loop ((n n) (ls names) (ret '()))
+      (if (or (not (number? n)) (= n 0)) ret
+        (loop (quotient n 1000)
+              (cdr ls)
+              (if (= (remainder n 1000) 0) ret
+                (append (name<1000 (remainder n 1000)) (if (null? (car ls)) ret
+                                                         (cons (car ls) ret)))))))))
+
+(define (count-letters ls)
+  (foldl (lambda (sum str)
+           (+ sum (string-length str)))
+         0 ls))
+
+(define (build-ls max)
+  (let loop ([n max])
+    (if (zero? n) '()
+      (cons (number-name n) (loop (sub1 n))))))
+
+(define (take-that-euler max)
+  (foldl + 0 
+         (map count-letters 
+              (build-ls max)))
--- euler/euler_9.adb
+++ euler/euler_9.adb
@@ -8,7 +8,8 @@ BEGIN
       FOR i IN n..(1000) LOOP
          IF float'remainder(sqrt(float(n**2 + i**2)), 1.0) = 0.0 THEN
             IF n+i+integer(sqrt(float(n**2 + i**2))) = 1000 THEN
-               put(n+i+integer(sqrt(float(n**2 + i**2))));                put(n*i*integer(sqrt(float(n**2 + i**2)))); new_line;
+               put(n+i+integer(sqrt(float(n**2 + i**2))));
+               put(n*i*integer(sqrt(float(n**2 + i**2)))); new_line;
                put(n); put(i); put(integer(sqrt(float(n**2 + i**2)))); new_line;
             END IF;
          END IF;
